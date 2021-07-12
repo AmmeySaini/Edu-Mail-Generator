411d32be439c411a71d3843747b4723db1ada10c
ammeysaini <AmmeySA1116@>
2021-07-12 15:31:48 +0530

    Fixed element not found

--- bot.py
+++ bot.py
@@ -650,6 +650,13 @@ def start_bot(start_url, email, college, collegeID):
             EC.element_to_be_clickable((By.ID, 'inputCaRes2YearsYes'))
         ).click()
 
+        element = driver.find_element_by_id('inputHomelessYouthNo')
+        desired_y = (element.size['height'] / 2) + element.location['y']
+        window_h = driver.execute_script('return window.innerHeight')
+        window_y = driver.execute_script('return window.pageYOffset')
+        current_y = (window_h / 2) + window_y
+        scroll_y_by = desired_y - current_y
+
         time.sleep(1)
 
         WebDriverWait(driver, 60).until(
