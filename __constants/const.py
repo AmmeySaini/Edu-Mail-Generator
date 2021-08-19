4fe35a8d470121bc59fff12c6407dc659ba2d979
abhishekhbhootwala <88439521+abhishekhbhootwala@users.noreply.github.com>
2021-08-20 04:29:37 +0600

    Update const.py

--- __constants/const.py
+++ __constants/const.py
@@ -9,9 +9,9 @@ import random
 
 start_url = 'https://www.opencccapply.net/gateway/apply?cccMisCode='
 
-clg_ids = ['941', '311', '361', '233', '431', '371', '091', '641', '231', '621', '521', '911', '345', '111', '411', '211', '811', '482', '373', '683']
+clg_ids = ['941', '311', '361', '233', '851', '431', '371', '091', '641', '231', '621', '521', '911', '345', '111', '411', '211', '811', '482', '373', '683']
 
-allColleges = ['MSJC College', 'Contra Costa College', 'City College', 'Sacramento College', 'Ohlone College', 'Cañada College', 'Southwestern College', 'Cuesta College', 'American River College', 'Antelope Valley College', 'Bakersfield College', 'Barstow Community College', 'Berkeley City College', 'Butte College', 'Cabrillo College', 'Calbright College', 'Cerritos College', 'Chabot College', 'Skyline College', 'Ventura College']
+allColleges = ['MSJC College', 'Contra Costa College', 'City College', 'Sacramento College', 'Mt San Antonio', 'Ohlone College', 'Cañada College', 'Southwestern College', 'Cuesta College', 'American River College', 'Antelope Valley College', 'Bakersfield College', 'Barstow Community College', 'Berkeley City College', 'Butte College', 'Cabrillo College', 'Calbright College', 'Cerritos College', 'Chabot College', 'Skyline College', 'Ventura College']
 
 country_codes = ['855', '561', '800', '325', '330', '229']
 
